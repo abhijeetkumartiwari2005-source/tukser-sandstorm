0e7d9e652ef8a8219009f9fe91dfc3c868edf050
abhijeet <abhijeetkumartiwari2005@gmail.com>
2026-09-03 12:39:21 +0530

    gitignore added

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ const io= new Server(httpServer,{
 });
   io.on('connection',(socket)=>{
    console.log(`user connected: ${socket.id}`);
-  socket.on('disconnection',()=>{
+  socket.on('disconnect',()=>{
     console.log(`user disconnected:${socket.id}`);
   });
 
